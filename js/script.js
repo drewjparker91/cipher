0af9d39702520b2777724882778afe65c00a40b1
William Donovan-Seid <wdstwo@live.com>
2020-08-10 16:46:33 -0700

    add script.js more messing around
    co-author: Drew Parker <drewparker91@gmail.com>
    co-author: Alma Frankenstein <a.q.frankenstein@gmail.com>

--- js/script.js
+++ js/script.js
@@ -3,18 +3,37 @@ $(document).ready(function() {
     return anonSent.charAt(0).toUpperCase();
   }
   function lastLetter(anonSent) {
-    return anonSent.charAt(str.length-1).toUpperCase();
+    return anonSent.charAt(anonSent.length-1).toUpperCase();
+  }
+  function reverseFL(firstL,lastL) {
+    return lastL + firstL;
+  }
+  function numberL(anonSent) {
+    let a = parseInt(anonSent.length).toFixed;
+    return a / 2;
   }
 
   const anonSent = prompt("Enter a sentence.");
   const firstL = firstLetter(anonSent);
   const lastL = lastLetter(anonSent);
+  const rFL = reverseFL(firstL,lastL);
+  const numL = numberL(anonSent);
+  const midL = anonSent.charAt(numL);
+  
+  alert(midL + anonSent + rFL);
 
-  anonSent.charAt(0).toUpperCase();
+});
+
+
+
+anonSent.charAt(0).toUpperCase();
   anonSent.slice(1);
   anonSent.charAt(-1).toUpperCase();
-    
 
+  alert(lastL);
 
-  alert(firstL);
-});
+  function middle(anonSent) {
+    return anonSent.slice(1);
+  }
+
+  
