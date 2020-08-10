ece1c11cb8e0c422749a1629755dca900c8379f8
William Donovan-Seid <wdstwo@live.com>
2020-08-10 15:36:03 -0700

    add script.js messed up code
    co-author: Drew Parker <drewparker91@gmail.com>
    co-author: Alma Frankenstein <a.q.frankenstein@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  function firstLetter(anonSent) {
+    return anonSent.charAt(0).toUpperCase();
+  }
+  function lastLetter(anonSent) {
+    return anonSent.charAt(str.length-1).toUpperCase();
+  }
+
   const anonSent = prompt("Enter a sentence.");
-  return anonSent
+  const firstL = firstLetter(anonSent);
+  const lastL = lastLetter(anonSent);
+
+  anonSent.charAt(0).toUpperCase();
+  anonSent.slice(1);
+  anonSent.charAt(-1).toUpperCase();
+    
+
+
+  alert(firstL);
 });
